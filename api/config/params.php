12439f6458b35845d47aa711d64d0e42410c9e2f
joseph@gmail.com <joseph@tafutaa.com>
2019-07-28 11:35:57 +0300

    initial Commit

--- api/config/params.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-	'adminEmail' => 'admin@example.com',
-	'documentpath'=> 'C:\\inetpub\\wwwroot\\callcenter\\api\\web\\upload\\', // Main Document Repository
-	'MAX_FILE_SIZE' => 30000000,
-];
