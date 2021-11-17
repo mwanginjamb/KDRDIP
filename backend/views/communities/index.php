f52a707a22fb6d6a5d2731273831669b7d821750
mwanginjamb <francnjamb@gmail.com>
2021-11-17 07:47:07 +0300

    Fix: Typo: Corrected Title of Communities  to include -CPMCs -  in it

--- backend/views/communities/index.php
+++ backend/views/communities/index.php
@@ -6,7 +6,7 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Communities';
+$this->title = 'Communities /  CPMCs';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <section id="configuration">
