93891a059a0e7405911df13cd56c357555c99456
mwanginjamb <fnjambi@outlook.com>
2022-01-28 10:57:32 +0300

    Fix: Adjust auth timeout to 20 min

--- backend/config/main.php
+++ backend/config/main.php
@@ -29,12 +29,12 @@ return [
 			'identityClass' => 'common\models\User',
 			'enableAutoLogin' => false,
 			'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
-            'authTimeout' => 60*5
+            'authTimeout' => 60*20 // If user is inactive for 10 min , log them out
 		],
 		'session' => [
 			// this is the name of the session cookie used for login on the backend
 			'name' => 'advanced-backend',
-            'timeout' => 10*60, // If user is inactive for 10 min , log them out
+            //'timeout' => 60*20, // If user is inactive for 10 min , log them out
 		],
 		'log' => [
 			'traceLevel' => YII_DEBUG ? 3 : 0,
