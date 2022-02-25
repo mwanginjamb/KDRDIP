531b8b1ddb39abb3ab83c4a8b47f6dfc92108616
mwanginjamb <fnjambi@outlook.com>
2022-02-25 09:45:57 +0300

    Feat: Add Groups API

---
+++ backend/modules/api/controllers/GroupsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace backend\modules\api\controllers;
+
+use app\models\Organizations;
+use backend\modules\api\resources\ProjectResource;
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\Cors;
+
+
+class GroupsController extends \yii\rest\ActiveController
+{
+    public $modelClass = Organizations::class;
+
+    public function behaviors(){
+        $behaviors = parent::behaviors();
+        $behaviours['cors'] = [
+            'class' => Cors::class,
+            'cors' => [
+                //  methods to allow
+                'Access-Control-Request-Method' => ['POST', 'PUT', 'GET','DELETE'],
+                // Allow only headers 'X-Wsse'
+                'Access-Control-Request-Headers' => ['*'],
+                // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
+                'Access-Control-Allow-Credentials' => false,
+                // Allow OPTIONS caching
+                'Access-Control-Max-Age' => 3600,
+                // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+            ],
+        ];
+
+        $behaviors['authenticator'] = [
+            'class' => HttpBearerAuth::class
+        ];
+
+
+        return $behaviors;
+    }
+}
