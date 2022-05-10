d360ad7893d107a35c624b3908785996d0cb1645
mwanginjamb <fnjambi@outlook.com>
2022-05-10 12:45:32 +0300

    Feat: make  project and organizations dropdowns searchable on bank account form

--- backend/views/bank-accounts/_form.php
+++ backend/views/bank-accounts/_form.php
@@ -11,7 +11,7 @@ $baseUrl = Yii::$app->request->baseUrl;
 ?>
 <script src="<?= $baseUrl; ?>/app-assets/js/jquery.min.js"></script>
 <script>
-	$( document ).ready(function() {
+	$(document).ready(function() {
 		manageType()
 	});
 
@@ -20,31 +20,30 @@ $baseUrl = Yii::$app->request->baseUrl;
 		if (typeid == 1) {
 			$("#countyList").hide();
 			$("#organizationList").hide();
-            $("#projectList").hide();
+			$("#projectList").hide();
 		} else if (typeid == 2) {
 			$("#countyList").show();
 			$("#organizationList").hide();
-            $("#projectList").hide();
+			$("#projectList").hide();
 		} else if (typeid == 3) {
 			$("#countyList").show();
 			$("#organizationList").show();
-            $("#projectList").hide();
-		}else if (typeid == 4) {
+			$("#projectList").hide();
+		} else if (typeid == 4) {
 			$("#countyList").show();
 			$("#projectList").show();
-            $("#organizationList").hide();
+			$("#organizationList").hide();
 		} else {
 			$("#countyList").hide();
 			$("#organizationList").hide();
-            $("#projectList").hide();
+			$("#projectList").hide();
 		}
 	}
-
 </script>
 <div class="card">
 	<div class="card-header">
 		<h4 class="form-section"><?= $this->title; ?></h4>
-		
+
 		<a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
 		<div class="heading-elements">
 			<ul class="list-inline mb-0">
@@ -58,51 +57,57 @@ $baseUrl = Yii::$app->request->baseUrl;
 	<div class="card-content collapse show">
 		<div class="card-body">
 			<?php $form = ActiveForm::begin(); ?>
-	 
-	 		<div class="row">
+
+			<div class="row">
 				<div class="col-md-6">
 					<?= $form->field($model, 'AccountNumber')->textInput(['maxlength' => true]) ?>
 				</div>
 				<div class="col-md-6">
 					<?= $form->field($model, 'AccountName')->textInput(['maxlength' => true]) ?>
-				</div>			
+				</div>
 			</div>
 
 			<div class="row">
 				<div class="col-md-6">
-					<?= $form->field($model, 'BankID')->dropDownList($banks, ['prompt'=>'Select...',
-					'onchange' => '$.post( "' . Yii::$app->urlManager->createUrl('bank-accounts/branches?id=') . '" + $(this).val(), function( data ) {
+					<?= $form->field($model, 'BankID')->dropDownList($banks, [
+						'prompt' => 'Select...',
+						'onchange' => '$.post( "' . Yii::$app->urlManager->createUrl('bank-accounts/branches?id=') . '" + $(this).val(), function( data ) {
 						$( "select#bankaccounts-branchid" ).html( data );
 					});
-					']) ?>
+					'
+					]) ?>
 				</div>
 				<div class="col-md-6">
-					<?= $form->field($model, 'BranchID')->dropDownList($bankBranches, ['prompt'=>'Select...']) ?>
-				</div>			
+					<?= $form->field($model, 'BranchID')->dropDownList($bankBranches, ['prompt' => 'Select...']) ?>
+				</div>
 			</div>
 
 			<div class="row">
 				<div class="col-md-6">
-					<?= $form->field($model, 'BankTypeID')->dropDownList($bankTypes, ['prompt'=>'Select...',
-					'onchange' => 'manageType()']) ?>
+					<?= $form->field($model, 'BankTypeID')->dropDownList($bankTypes, [
+						'prompt' => 'Select...',
+						'onchange' => 'manageType()'
+					]) ?>
 				</div>
 				<div class="col-md-6" id="countyList">
-                    <?= $form->field($model, 'CountyID')->dropDownList($counties, ['prompt' => 'Select...', 'class' => 'form-control select2',
-                        'onchange' => '
+					<?= $form->field($model, 'CountyID')->dropDownList($counties, [
+						'prompt' => 'Select...', 'class' => 'form-control select2',
+						'onchange' => '
                         $.post( "' . Yii::$app->urlManager->createUrl('counties/projects?id=') . '"+$(this).val(), function( data ) {
                             $( "select#bankaccounts-projectid" ).html( data );
                         });
                         $.post( "' . Yii::$app->urlManager->createUrl('counties/organizations?id=') . '"+$(this).val(), function( data ) {
                             $( "select#bankaccounts-organizationid" ).html( data );
                         });
-                    ']) ?>
+                    '
+					]) ?>
 				</div>
-                <div class="col-md-6" id="organizationList">
-					<?= $form->field($model, 'OrganizationID')->dropDownList($organizations, ['prompt'=>'Select...']) ?>
+				<div class="col-md-6" id="organizationList">
+					<?= $form->field($model, 'OrganizationID')->dropDownList($organizations, ['prompt' => 'Select...']) ?>
 				</div>
-                <div class="col-md-6" id="projectList">
-					<?= $form->field($model, 'ProjectID')->dropDownList($projects, ['prompt'=>'Select...']) ?>
-				</div>			
+				<div class="col-md-6" id="projectList">
+					<?= $form->field($model, 'ProjectID')->dropDownList($projects, ['prompt' => 'Select...']) ?>
+				</div>
 			</div>
 
 			<div class="row">
@@ -110,7 +115,7 @@ $baseUrl = Yii::$app->request->baseUrl;
 					<?= $form->field($model, 'Notes')->textarea(['rows' => 6]) ?>
 				</div>
 				<div class="col-md-6">
-				</div>			
+				</div>
 			</div>
 
 			<div class="form-group">
@@ -120,5 +125,12 @@ $baseUrl = Yii::$app->request->baseUrl;
 
 			<?php ActiveForm::end(); ?>
 		</div>
-	 </div>
+	</div>
 </div>
+
+<?php
+$script = <<<JS
+$('#bankaccounts-projectid,#bankaccounts-organizationid').select2();
+JS;
+
+$this->registerJs($script, \yii\web\View::POS_END);
